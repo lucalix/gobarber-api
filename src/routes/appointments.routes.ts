d779cd5b41a9be1bdc02d05a4cde8d9e8a78a060
Lucas Calixto <lucas.float@gmail.com>
2020-05-04 22:21:43 -0300

    Adiciona agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,22 +1,38 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+    const parsedDate = parseISO(date);
 
-  appointments.push(appointment);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  return response.json(appointment);
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('API is running on port 3333');
+  console.log('✳️ API is running on port 3333 ✳️');
 });
